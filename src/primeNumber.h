e24dddeb01a845d2851e42327390726a7ba7db6e
Barbe Bryan <bryan.barbe01@gmail.com>
2023-11-18 03:08:35 +0100

    rename more accurate

--- src/primeNumber.h
+++ src/primeNumber.h
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-vector<unsigned long> primeList(unsigned long limit){
+vector<unsigned long> primeListUpTo(unsigned long limit){
     vector<unsigned long> primes = {2};
     bool isPrime = false;
     for(unsigned long i = 3; i < limit; i += 2 ){
+        //printf("\r %d", i);
         isPrime = true;
         for(unsigned long p : primes){
+            if((double)p > ceil(sqrt(i))) break;
             if(i % p == 0){
                 isPrime = false;
                 break;
@@ -15,16 +18,19 @@ vector<unsigned long> primeList(unsigned long limit){
         }
         if(isPrime) primes.push_back(i);
     }
+    //printf("\n");
     return primes;
 }
 
-vector<unsigned long> primeListUpTo(unsigned long limit){
+vector<unsigned long> primeList(unsigned long limit){
     vector<unsigned long> primes = {2};
     bool isPrime = false;
     int i = 3;
     while(primes.size() < limit){
+        //printf("\r %d", i);
         isPrime = true;
         for(unsigned long p : primes){
+            if((double)p > ceil(sqrt(i))) break;
             if(i % p == 0){
                 isPrime = false;
                 break;
@@ -34,5 +40,6 @@ vector<unsigned long> primeListUpTo(unsigned long limit){
         if(isPrime) primes.push_back(i);
         i+=2;
     }
+    //printf("\n");
     return primes;
 }
